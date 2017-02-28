319f87e35222669e2b8633ed0586b67b78621c6e
DerrickF <Derrick.w.fink@gmail.com>
2017-02-27 23:06:03 -0700

    added grunt serve to host up and do live reloads

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,39 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        //Minify Files
         uglify: {
             main: {
                 src: 'js/<%= pkg.name %>.js',
                 dest: 'js/<%= pkg.name %>.min.js'
             }
         },
+        //Copy Files To Release Folder
         copy: {
             main: {
-                    files: [
+                files: [
                     // includes files within path
-                    {expand: true, src: ['index.html','css/*.min.css','font-awesome/**','fonts/**', 'img/**','js/*.min.js',], dest: 'release'},
-                ]},
+                    { expand: true, src: ['index.html', 'css/*.min.css', 'font-awesome/**', 'fonts/**', 'img/**', 'js/**.min.js',], dest: 'release' },
+                ]
+            },
         },
+        //serve it up
+        connect: {
+            server: {
+                options: {
+                    port: 8000,
+                    base: "",
+                    livereload: true,
+                    hostname:'localhost',
+                    open:true
+                }
+            }
+        },
+        //Clean Release Folder
         clean: ["release/**"],
+        //Compile and minify the less files
         less: {
             expanded: {
                 options: {
@@ -36,11 +53,12 @@ module.exports = function(grunt) {
                 }
             }
         },
+        //Banner for minified code
         banner: '/*!\n' +
-            ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-            ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
-            ' */\n',
+        ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+        ' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
+        ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+        ' */\n',
         usebanner: {
             dist: {
                 options: {
@@ -52,6 +70,7 @@ module.exports = function(grunt) {
                 }
             }
         },
+        //Watch for change
         watch: {
             scripts: {
                 files: ['js/<%= pkg.name %>.js'],
@@ -67,6 +86,13 @@ module.exports = function(grunt) {
                     spawn: false,
                 }
             },
+            options:{
+                livereload: true
+            },
+            html: {
+            files: ['index.html'],
+            tasks: ['default']
+        },
         },
     });
 
@@ -77,9 +103,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     // Default task(s).
     grunt.registerTask('default', ['uglify', 'less', 'usebanner']);
-    grunt.registerTask('build-release',['clean','default','copy']);
+    grunt.registerTask('serve',['default','connect','watch']);
 
 };
